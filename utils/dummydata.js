d0f4b571a2e1cf241a955d4b85e0fcc680cdeb7b
vedavyas <vedavyas14042003@gmail.com>
2023-09-27 13:00:05 +0530

    pushed to github

---
+++ utils/dummydata.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: "123",
+    code: "123",
+    name: "abc",
+    description: "abc",
+    image: "http://example.com",
+    price: 2,
+    category: "a",
+    quantity: 2,
+    inventoryStatus: "available",
+    rating: 3,
+  },
+  {
+    id: "123",
+    code: "123",
+    name: "abc",
+    description: "abc",
+    image: "http://example.com",
+    price: 2,
+    category: "a",
+    quantity: 2,
+    inventoryStatus: "available",
+    rating: 3,
+  },
+];
+
+export default data;
+
+// id: string;
+// code: string;
+// name: string;
+// description: string;
+// image: string;
+// price: number;
+// category: string;
+// quantity: number;
+// inventoryStatus: string;
+// rating: number;
